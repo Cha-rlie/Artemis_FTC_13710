3150139f6eebd360abd69bb8b7f4036181fb65c0
Cha_rlie <c7ts2charliehome@outlook.com>
2023-03-14 00:01:12 +1000

    Basic Functionality and Viper Slide Testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Artemis_TeleOP.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+// Import the necessary modules and classes
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@TeleOp
+
+public class Artemis_TeleOP extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        // Initialising the motors up for drive base
+        SampleMecanumDrive driveBase = new SampleMecanumDrive(hardwareMap);
+
+        DcMotorEx UpLeft = hardwareMap.get(DcMotorEx.class, "UpLeft");
+        DcMotorEx UpRight = hardwareMap.get(DcMotorEx.class, "UpRight");
+        DcMotorEx DownLeft = hardwareMap.get(DcMotorEx.class, "DownLeft");
+        DcMotorEx DownRight = hardwareMap.get(DcMotorEx.class, "DownRight");
+
+        // Initialising the motors for the dual-intake system
+        DcMotorEx IntakeLeft = hardwareMap.get(DcMotorEx.class, "IntakeLeft");
+        DcMotorEx IntakeRight = hardwareMap.get(DcMotorEx.class, "IntakeRight");
+        DcMotorEx DepositLeft = hardwareMap.get(DcMotorEx.class, "DepositLeft");
+        DcMotorEx DepositRight = hardwareMap.get(DcMotorEx.class, "DepositRight");
+
+        // Initialising the servos
+        Servo ClawServo = hardwareMap.get(Servo.class, "ClawServo");
+
+        // Communicating with the driver station
+        telemetry.addData("Status", "Motors and Servos Initialised");
+
+        // Wait for the driver to click the "Play" button before proceeding
+        waitForStart();
+
+        // Run until the "Stop" button is pressed
+        while (opModeIsActive()) {
+
+            telemetry.addData("Status", "TeleOp Running");
+
+            boolean inputLeftStatus = this.gamepad2.a;
+            float inputLeftManual = this.gamepad2.left_stick_x;
+
+        }
+
+    }
+
+}
