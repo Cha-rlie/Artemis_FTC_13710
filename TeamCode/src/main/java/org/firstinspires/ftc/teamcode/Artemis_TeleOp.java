f955aa73159340967ee9be8caaaa38bb9a203e2a
Cha_rlie <c7ts2charliehome@outlook.com>
2023-04-06 20:48:18 +1000

    Add Manual Intake and Deposit System Contol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Artemis_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Artemis_TeleOp.java
@@ -46,25 +46,44 @@ public class Artemis_TeleOp extends LinearOpMode {
             telemetry.addData("Status", "TeleOp Running");
 
             // Initialise the input values from first controller for the drive base
-            float XInput = (float)(this.gamepad1.right_stick_x*1.1); // Account for imperfect strafing
-            float YInput = -this.gamepad1.left_stick_y; // One side needs to be reversed
-            float RInput = this.gamepad1.right_stick_x;
+            float xInput = (float)(this.gamepad1.right_stick_x*1.1); // Account for imperfect strafing
+            float yInput = -this.gamepad1.left_stick_y; // One side needs to be reversed
+            float rInput = this.gamepad1.right_stick_x;
 
             // Initialise the input values from the second controller for the dual intake system
-            boolean inputLeftStatus = this.gamepad2.a;
-            int inputLeftManual = (int)this.gamepad2.left_stick_x;
+            //boolean inputLeftStatus = this.gamepad2.a;
+            boolean intakeDepositStatus = true;
+            float leftIntakeInput = this.gamepad2.left_stick_y;
+            float rightIntakeInput = this.gamepad2.right_stick_y;
+            float leftDepositInput = this.gamepad2.left_stick_y;
+            float rightDepositInput = this.gamepad2.right_stick_y;
+
 
             // Returns the largest denominator that the power of the motors must be divided by to keep their original ratio
-            double ratioScalingDenominator = Math.max(Math.abs(YInput) + Math.abs(XInput) + Math.abs(RInput), 1);
+            double ratioScalingDenominator = Math.max(Math.abs(yInput) + Math.abs(xInput) + Math.abs(rInput), 1);
 
             // Drive the drive base with mecanum code
-            UpLeft.setPower((YInput + XInput + RInput) / ratioScalingDenominator);
-            UpRight.setPower((YInput - XInput - RInput) / ratioScalingDenominator);
-            DownLeft.setPower((YInput - XInput + RInput) / ratioScalingDenominator);
-            DownRight.setPower((YInput + XInput - RInput) / ratioScalingDenominator);
+            UpLeft.setPower((yInput + xInput + rInput) / ratioScalingDenominator);
+            UpRight.setPower((yInput - xInput - rInput) / ratioScalingDenominator);
+            DownLeft.setPower((yInput - xInput + rInput) / ratioScalingDenominator);
+            DownRight.setPower((yInput + xInput - rInput) / ratioScalingDenominator);
 
-            // Code to test the left intake motor
-            IntakeLeft.setPower(inputLeftManual);
+            // Code to test the MANUALLY test the intake and deposit systems
+            if (this.gamepad2.a) {
+                intakeDepositStatus = true;
+            }
+            if (this.gamepad2.b) {
+                intakeDepositStatus = false;
+            }
+
+            if (intakeDepositStatus) {
+                IntakeLeft.setPower(leftIntakeInput);
+                IntakeRight.setPower(rightIntakeInput);
+            }
+            else {
+                DepositLeft.setPower(leftDepositInput);
+                DepositRight.setPower(rightDepositInput);
+            }
 
         }
 
