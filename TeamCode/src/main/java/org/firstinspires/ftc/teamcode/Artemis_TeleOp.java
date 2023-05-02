273322e71e573f2b1d3305586df930045d0161e9
Xlectron <joshuawaghorn@hotmail.com>
2023-05-02 19:01:05 +1000

    Intake Slides Functional

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Artemis_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Artemis_TeleOp.java
@@ -28,28 +28,40 @@ public class Artemis_TeleOp extends LinearOpMode {
         double RotateClaw_HomePos = 0.631; // Position where RotateClaw is ready for intaking
 
         double ClosedClawPos = 0.116; // Position where claw is closed
-        double OpenClawPos = 0.2694; // Position where claw is open
+        double OpenClawPos = 0.2994; // Position where claw is open
 
         double ClawFowardPos = 0;
         double ClawBackwardsPos = 0.7;
+
+        int IntakeHome = 955; // Fully contracted position
+        int IntakeOut = -1050; // Fully extended position
 
 
         // Initialising the motors up for drive base
 
         DcMotorEx FrontLeft = hardwareMap.get(DcMotorEx.class, "FrontLeft");
-        FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         DcMotorEx FrontRight = hardwareMap.get(DcMotorEx.class, "FrontRight");
         DcMotorEx RearLeft = hardwareMap.get(DcMotorEx.class, "RearLeft");
+        RearLeft.setDirection(DcMotor.Direction.REVERSE);
         DcMotorEx RearRight = hardwareMap.get(DcMotorEx.class, "RearRight");
 
+
         // Initialising the motors for the dual-intake system
-        /*
+
         DcMotorEx IntakeLeft = hardwareMap.get(DcMotorEx.class, "IntakeLeft");
         IntakeLeft.setDirection(DcMotorEx.Direction.REVERSE);
         DcMotorEx IntakeRight = hardwareMap.get(DcMotorEx.class, "IntakeRight");
+
+        IntakeLeft.setTargetPosition(IntakeHome);
+        IntakeRight.setTargetPosition(IntakeHome);
+        IntakeLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        IntakeRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+         /*
         DcMotorEx DepositLeft = hardwareMap.get(DcMotorEx.class, "DepositLeft");
         DcMotorEx DepositRight = hardwareMap.get(DcMotorEx.class, "DepositRight");
-        */
+*/
 
         // Initialising the servos
         Servo Claw = hardwareMap.get(Servo.class, "Claw");
@@ -59,11 +71,12 @@ public class Artemis_TeleOp extends LinearOpMode {
         Servo V4B_2 = hardwareMap.get(Servo.class, "V4B_2");
         //Servo Latch = hardwareMap.get(Servo.class, "Latch");
 
+        // Default Positions
         V4B_1.setPosition(V4B_1_HomePos);
         V4B_2.setPosition(V4B_2_HomePos);
         RotateClaw.setPosition(RotateClaw_HomePos);
         Claw.setPosition(ClosedClawPos);
-        SpinClaw.setPosition(ClawFowardPos);
+        // SpinClaw.setPosition(ClawFowardPos);
 
 
         // Communicating with the driver station
@@ -94,11 +107,6 @@ public class Artemis_TeleOp extends LinearOpMode {
             // Returns the largest denominator that the power of the motors must be divided by to keep their original ratio
             double ratioScalingDenominator = Math.max(Math.abs(yInput) + Math.abs(xInput) + Math.abs(rInput), 1);
 
-            // Drive the drive base with mecanum code
-            FrontLeft.setPower((yInput + xInput + rInput) / ratioScalingDenominator);
-            FrontRight.setPower((yInput - xInput - rInput) / ratioScalingDenominator);
-            RearLeft.setPower((yInput - xInput + rInput) / ratioScalingDenominator);
-            RearRight.setPower((yInput + xInput - rInput) / ratioScalingDenominator);
 
             // Drive the drive base with mecanum code
             FrontLeft.setPower((yInput + xInput + rInput) / ratioScalingDenominator);
@@ -108,10 +116,9 @@ public class Artemis_TeleOp extends LinearOpMode {
 
             // MANUALLY move the intake and deposit systems
 
-            /*
-            IntakeLeft.setPower(intakeInput / 2);
-            IntakeRight.setPower(intakeInput / 2);
-
+            IntakeLeft.setPower(intakeInput/2);
+            IntakeRight.setPower(intakeInput/2);
+ /*
             if (depositInputUp && depositInputDown) {}
             else if (depositInputUp) {
                 DepositLeft.setPower(0.5);
@@ -120,11 +127,24 @@ public class Artemis_TeleOp extends LinearOpMode {
                 DepositLeft.setPower(-0.5);
                 DepositRight.setPower(-0.5);
             }
+*/
 
             // Print out Encoder Values to the driver station
-            telemetry.addData("Intake",(int)((IntakeLeft.getCurrentPosition()+IntakeRight.getCurrentPosition())/2));
-            telemetry.addData("Deposit",(int)((DepositLeft.getCurrentPosition()+DepositRight.getCurrentPosition())/2));
-*/
+            telemetry.addData("IntakeL", IntakeLeft.getCurrentPosition());
+            telemetry.addData("IntakeR", IntakeRight.getCurrentPosition());
+           // telemetry.addData("Deposit",(int)((DepositLeft.getCurrentPosition()+DepositRight.getCurrentPosition())/2));
+
+//            if(gamepad2.left_stick_y > 0.5) {
+//                IntakeLeft.setTargetPosition(IntakeHome);
+//                IntakeRight.setTargetPosition(IntakeHome);
+//            } else if(gamepad2.left_stick_y < -0.5) {
+//                IntakeLeft.setTargetPosition(IntakeOut);
+//                IntakeRight.setTargetPosition(IntakeOut);
+//            } else {
+//                IntakeLeft.setTargetPosition(IntakeLeft.getCurrentPosition());
+//                IntakeRight.setTargetPosition(IntakeRight.getCurrentPosition());
+//            }
+//
 
 
             // Handling debounce issues
@@ -164,10 +184,8 @@ public class Artemis_TeleOp extends LinearOpMode {
                 V4B_2.setPosition(V4B_2_HomePos);
             }
 
-            SpinClaw.setPosition(SpinClaw.getPosition()+(V4BInput*2/200));
-
-//            V4B_1.setPosition(V4B_1.getPosition()+(V4BInput*2/200));
-//            V4B_2.setPosition(V4B_2.getPosition()-(V4BInput*2/200));
+            V4B_1.setPosition(V4B_1.getPosition()+(V4BInput*2/200));
+            V4B_2.setPosition(V4B_2.getPosition()-(V4BInput*2/200));
             telemetry.addData("V4B_1", V4B_1.getPosition());
             telemetry.addData("V4B_2", V4B_2.getPosition());
             telemetry.addData("RotateClaw", RotateClaw.getPosition());
